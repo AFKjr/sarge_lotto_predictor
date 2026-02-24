a3f7f4e94a6b348e4b586a20a1a4b65bb849a8fd
AFK_Jr <90282329+AFK Jr@users.noreply.github.com>
2026-02-23 20:59:20 -0600

    Created:
    created main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const calculateButton = document.getElementById("calculate-button");
+
+calculateButton.addEventListener("click", handleCalculation);
+
+function handleCalculation() {
+    console.log("button clicked");
+}
+
